6238736444bbb1fa9a9bbe351d8bea348a69ff4a
Vahit Emre <vemretellier@gmail.com>
2021-12-07 20:33:55 +0300

    First Commit

--- TravelAndPhoto/PhotoListVC.swift
+++ TravelAndPhoto/PhotoListVC.swift
@@ -6,24 +6,74 @@
 //
 
 import UIKit
+import CoreData
 
-class PhotoListVC: UIViewController {
+class PhotoListVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var titleArray = [String]()
+    var idArray = [UUID]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+//          topbar ulaşıldı ve UIBarButtonItem.SystemItem.add ile add butonu koyuldu.
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        getData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func getData(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+//        veri çekmeyi hızlandırmak içi cash ile ilgili işlem
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do{
+//            results is an Array
+            let results = try context.fetch(fetchRequest)
+            
+            if results.count > 0 {
+                self.titleArray.removeAll(keepingCapacity: false)
+                self.idArray.removeAll(keepingCapacity: false)
+                
+                for result in results as! [NSManagedObject]{
+                    if let title = result.value(forKey: "title") as? String{
+                        self.titleArray.append(title)
+                    }
+                    
+                    if let id = result.value(forKey: "id") as? UUID{
+                        self.idArray.append(id)
+                    }
+//                    tableView refresh
+                    tableView.reloadData()
+                }
+            }
+        } catch {
+            print("error!")
+        }
     }
-    */
+    
+    @objc func addButtonClicked(){
+        performSegue(withIdentifier: "toVC", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return idArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = titleArray[indexPath.row]
+        return cell
+    }
+
 
 }
--- TravelAndPhoto/ViewController.swift
+++ TravelAndPhoto/ViewController.swift
@@ -6,14 +6,131 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleTF: UITextField!
+    @IBOutlet weak var subtitleTF: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
+//    CLLocationManager should be used if doing something related to location
+    var locationManager = CLLocationManager()
+    
+    var choosenLatitude = Double()
+    var choosenLongitude = Double()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+
+        
+//        Delegates should be used for using them classes or function
+        mapView.delegate = self
+        locationManager.delegate = self
+//          desiredAccuracy should be used for accuracy in the map location
+//        the bes lacation accuracy is kCLLocationAccuracyBest
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+//        For user permission
+        locationManager.requestWhenInUseAuthorization()
+//        For take the user's location
+        locationManager.startUpdatingLocation()
+        
+        
+//        -------------------------RECOGNIZERS-----------------------------------
+        
+//        for pin
+        let gestureRecognizer =  UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation(gestureRecognizer:)))
+        gestureRecognizer.minimumPressDuration = 2
+        mapView.addGestureRecognizer(gestureRecognizer)
+        
+//        for close keyboard
+        let keyboardGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(keyboardGestureRecognizer)
+        
+        imageView.isUserInteractionEnabled = true
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageTapRecognizer)
+        
+    }
+    
+    @objc func selectImage(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info:[UIImagePickerController.InfoKey : Any]){
+//        belki kullanıcı cancele basar belki ımage değildi oyuzde as? kullanıldı
+        imageView.image = info[.originalImage] as? UIImage
+//        Closing
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func hideKeyboard(){
+//        editleme işleme bittikten sonra herhangi bir yere tıklanırsa birşey açıksa kapanacaktır
+        view.endEditing(true)
+    }
+    
+    @IBAction func SaveButtonClicked(_ sender: Any) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newPlaces = NSEntityDescription.insertNewObject(forEntityName: "Places", into: context)
+        
+        newPlaces.setValue(titleTF.text, forKey: "title")
+        newPlaces.setValue(subtitleTF.text, forKey: "subtitle")
+        newPlaces.setValue(choosenLatitude, forKey: "latitude")
+        newPlaces.setValue(choosenLongitude, forKey: "longitude")
+        newPlaces.setValue(UUID(), forKey: "id")
+        
+        do{
+            try context.save()
+            print("Success")
+        }catch{
+            print("Error!")
+        }
+        
+    }
+    @objc func chooseLocation(gestureRecognizer : UILongPressGestureRecognizer){
+//        Added gestureRecognizer to use defined functions
+//        if gestureRecognizer is began, in other words user touch point in the map
+        if gestureRecognizer.state == .began {
+//            kullanıcıın dokunduğu locasyon alındı ve koordinata çevrildi sonra annatation oluşturulup mapview e eklendi
+            let touchedLocation = gestureRecognizer.location(in: self.mapView)
+//          touchedCoordinate give coordinate which toched by user
+            let touchedCoordinate = mapView.convert(touchedLocation, toCoordinateFrom: self.mapView)
+            
+            choosenLatitude = touchedCoordinate.latitude
+            choosenLongitude = touchedCoordinate.longitude
+            
+//            for add Pin preferences
+            let annatation = MKPointAnnotation()
+            annatation.coordinate = touchedCoordinate
+            annatation.title = titleTF.text
+            annatation.subtitle = subtitleTF.text
+            mapView.addAnnotation(annatation)
+        }
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        didUpdateLocations locations: [CLLocation] is the updating location array
+//        CLLocation enlem ve boylam objesi
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+//        for focus
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        mapView.setRegion(region, animated: true)
+    }
 
 }
 
